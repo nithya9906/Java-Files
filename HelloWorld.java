4fdf397973a8cc8aba3cc03981e8cf4e0b54f5d1
nithya9906 <nithya9906@example.com>
2021-06-14 17:45:56 +0530

    First java program

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld{
+	public static void main(String ...args){//using variables arguments or var-args
+		String firstName=args[0];
+		String lastName=args[1];
+		int age=Integer.parseInt(args[2]);
+		System.out.println("Welcome to Java "+firstName+" "+lastName);
+		System.out.println("Line 2 ...");
+
+	}
+}
+
+
+
+
+//in
+//err
