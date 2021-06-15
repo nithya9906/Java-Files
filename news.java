48fa887752a3c773755997b7ee685d73315e3877
nithya9906 <nithya9906@example.com>
2021-06-15 10:31:22 +0530

    First

---
+++ news.java
@@ -0,0 +1,13 @@
+System.out.println("Example of continue statement:");
+		System.out.println("Print all numbers from 1 to 10 except multiple of 5:");
+		for(int j=1;j<11;i++)
+		{
+			if(j==5 | j==10)
+			{
+				continue;
+			}
+			else
+			{
+				System.out.println(j);
+			}
+		}
